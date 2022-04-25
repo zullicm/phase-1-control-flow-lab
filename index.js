fc9a7edf9cc0467e96e65510d0d1a408a9d3d37e
Christopher Marzulli <chriszulli15@gmail.com>
2022-04-25 12:26:33 -0400

    commit tree 4e0f747d5265cd943cbcf87fe2fc24168c6a160c

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!'
+  }else if (feet <= 2000){
+    return 'That will be twenty bucks.'
+  }else if (feet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
